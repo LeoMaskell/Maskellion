de1eb4babeeba0bd138a351a16c824c47f687ef5
Leo Maskell <22maskelll@bluecoatstudent.org.uk>
2025-08-14 11:06:21 +0000

    sorted icons but git keeps crashing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT || 10000;
 
-app.use(express.JSON());
+app.use(express.json());
 
 
 // pages
@@ -17,6 +17,10 @@ app.get('/lib.html', (req, res) => {
     res.sendFile(__dirname + '/frontend/lib.html');
 });
 
+app.get('/search.html', (req, res) => {
+    res.sendFile(__dirname + '/frontend/search.html');
+});
+
 app.get('/settings.html', (req, res) => {
     res.sendFile(__dirname + '/frontend/settings.html');
 });
@@ -25,6 +29,7 @@ app.get('/song.html', (req, res) => {
     res.sendFile(__dirname + '/frontend/song.html');
 });
 
+
 //static files
 app.get('/style.css', (req, res) => {
     res.sendFile(__dirname + '/static/logo.png');
@@ -32,6 +37,38 @@ app.get('/style.css', (req, res) => {
 
 app.get('/logo', (req, res) => {
     res.sendFile(__dirname + '/static/logo.png');
+});
+// icons
+app.get('/book', (req, res) => {
+    res.sendFile(__dirname + '/static/icons/book.png');
+});
+
+app.get('/bookH', (req, res) => {
+    res.sendFile(__dirname + '/static/icons/bookH.png');
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/static/icons/home.png');
+});
+
+app.get('/homeH', (req, res) => {
+    res.sendFile(__dirname + '/static/icons/homeH.png');
+});
+
+app.get('/search', (req, res) => {
+    res.sendFile(__dirname + '/static/icons/search.png');
+});
+
+app.get('/searchH', (req, res) => {
+    res.sendFile(__dirname + '/static/icons/searchH.png');
+});
+
+app.get('/settings', (req, res) => {
+    res.sendFile(__dirname + '/static/icons/settings.png');
+});
+
+app.get('/settingsH', (req, res) => {
+    res.sendFile(__dirname + '/static/icons/settingsH.png');
 });
 
 
